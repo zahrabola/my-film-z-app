1e85659a59fc3bca5fc4dfd4a39f78305f5c2fa8
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-04-07 15:08:47 +0100

    update- search box

--- src/App.js
+++ src/App.js
@@ -2,29 +2,39 @@ import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
  const [movies, setMovies] = useState([]);
+ const [searchValue, setSearchValue] = useState("");
 
 
-const getMovieRequest = async () => {
-  const url = `http://www.omdbapi.com/?s=avengers&apikey=21bfc730`;
+const getMovieRequest = async (searchValue) => {
+  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=21bfc730`;
 
   const response = await fetch(url);
   const responseJson = await response.json();
-  console.log(responseJson);
+  if (responseJson.Search) {
     setMovies(responseJson.Search);
-
+  }
 };
 
   useEffect(() => {
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
  return (
-   <div className="container-fluid movie-app">
+   <div className="container-fluid justify-content-start movie-app">
      <h1>Hello</h1>
-     <div className="row d-flex align-items-center mt-4 mb-4">
+     <div className="row">
+       <MovieListHeading heading="Movies" />
+       <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+     </div>
+     <div class="container">
+     <div className="row">
        <MovieList movies={movies} />
+     </div>
      </div>
    </div>
  );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,10 +4,12 @@ const MovieList = (props) => {
 return (
   <>
     {props.movies.map((movie, index) => (
-      <div className="image-container d-flex justify-content-start m-3">
-   <img src={movie.Poster} alt='movie'></img>
-          {" "}
-       
+      <div className="image-container">
+        <img src={movie.Poster} alt="movie" id="movie-poster"></img>
+        <div
+          onClick={() => props.handleFavouritesClick(movie)}
+          className="overlay d-flex align-items-center justify-content-center"
+        ></div>
       </div>
     ))}
   </>
